a36f99ae0cf932b4e3e314d1bff273c3b34970fd
Radoslav Pavlinchov <radoslav.pavlinchov@gmail.com>
2020-05-01 23:14:27 +0300

    Created a new project with express-generator

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
-const ProgressBar = require('progress')
+const ProgressBar = require('progress');
 
 const hostname = '127.0.0.1';
 const port = process.env.port || 3000;
@@ -15,48 +15,6 @@ const timer = setInterval(() => {
 }, 100);
 
 const server = http.createServer((req, res) => {
-  // if (req.url === '/') {
-  //   fs.readFile(
-  //     path.join(__dirname, 'public', 'index.html'),
-  //     (err, content) => {
-  //       if (err) throw err;
-  //       res.statusCode = 200;
-  //       res.setHeader('Content-Type', 'text/html');
-  //       res.end(content);
-  //     }
-  //   )
-  // } else if (req.url === '/about') {
-  //   fs.readFile(
-  //     path.join(__dirname, 'public', 'about.html'),
-  //     (err, content) => {
-  //       if (err) throw err;
-  //       res.statusCode = 200;
-  //       res.setHeader('Content-Type', 'text/html');
-  //       res.end(content);
-  //     }
-  //   )
-  // } else if (req.url === '/contact-me') {
-  //   fs.readFile(
-  //     path.join(__dirname, 'public', 'contact-me.html'),
-  //     (err, content) => {
-  //       if (err) throw err;
-  //       res.statusCode = 200;
-  //       res.setHeader('Content-Type', 'text/html');
-  //       res.end(content);
-  //     }
-  //   )
-  // } else {
-  //   fs.readFile(
-  //     path.join(__dirname, 'public', 'notFound.html'),
-  //     (err, content) => {
-  //       if (err) throw err;
-  //       res.statusCode = 200;
-  //       res.setHeader('Content-Type', 'text/html');
-  //       res.end(content);
-  //     }
-  //   )
-  // } 
-
   // Build Path
   let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
 
@@ -66,18 +24,6 @@ const server = http.createServer((req, res) => {
   // Initial content type
   let contentType = 'text/html';
 
-  // Check extension and set content type
-  // if (extName === '.js') {
-  //   contentType = "text/javascript";
-  // } else if (extName === ".json") {
-  //   contentType = "application/json";
-  // } else if (extName === ".css") {
-  //   contentType = "text/css";
-  // } else if (extName === ".png") {
-  //   contentType = "image/png";
-  // } else if (extName === ".jpg") {
-  //   contentType = "image/jpg";
-  // }
   switch (extName) {
     case ".js":
       contentType = "text/javascript";
