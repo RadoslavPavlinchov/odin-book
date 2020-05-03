889c35678ed10b25362e9ecbb47ad926eddcd7cf
Radoslav Pavlinchov <radoslav.pavlinchov@gmail.com>
2020-05-03 23:00:38 +0300

    Created a new form view

--- localLibrary/app.js
+++ localLibrary/app.js
@@ -6,13 +6,14 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var newRouter = require('./routes/new');
 
 var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
 var mongoDB = 'mongodb+srv://admin:123pass123@cluster0-yq9z2.azure.mongodb.net/local_library?retryWrites=true&w=majority';
-mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/new', newRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ localLibrary/routes/new.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+let messages = [
+    {
+        message: 'Hi there',
+        author: 'Amando',
+        added: new Date()
+    },
+    {
+        message: 'Hello World',
+        author: 'Charles',
+        added: new Date()
+    },
+]
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    res.render('form', {
+        title: 'Mini Message Board',
+        messages: messages
+    });
+});
+
+router.post('/', function (req, res, next) {
+    const { author, message } = req.body;
+    const added = new Date();
+    messages.push({
+        author,
+        message,
+        added
+    });
+    res.redirect('/new')
+});
+
+module.exports = router;
